c9351f8a6878d27ac48815c64f77d338acb754b5
my-spock <rplowman15@gmail.com>
2016-04-22 09:20:39 -0600

    update playerService

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-var name = jQuery('input[name=playerName]').text();
-var position = jQuery('input[name=playerPosition]').text();
-var number = jQuery('input[name=playerNumber]').text();
+var name = $('input[name=playerName]').name();
+var position = $('input[name=playerPosition]').name();
+var number = $('input[name=playerNumber]').name();
+var imgUrl = "http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/"
+
+var image = $('<img>').attr('href', imgUrl);
 
 $('form').on('submit', function() {
     event.preventDefault();
     
     console.log(name, position, number);
+    
+    
 })
